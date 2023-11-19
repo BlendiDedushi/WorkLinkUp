7f1d4183dbeee687aedfbfe54872fad382373b61
blendi <blendidedushi@gmail.com>
2023-11-19 22:56:10 +0100

    Enable profile photo management in Jetstream

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -76,9 +76,16 @@
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
                             @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-                                <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
-                                    <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
-                                </button>
+                            <button
+                                class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                                @if(!empty(auth()->user()->profile_photo_path))
+                                <img src="{{ asset('storage/'.auth()->user()->profile_photo_path) }}"
+                                    alt="{{ auth()->user()->name }}" class="h-8 w-8 rounded-full object-cover">
+                                @else
+                                <img src="{{ auth()->user()->profile_photo_url }}" alt="{{ auth()->user()->name }}"
+                                    class="h-8 w-8 rounded-full object-cover">
+                                @endif
+                            </button>
                             @else
                                 <span class="inline-flex rounded-md">
                                     <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
@@ -148,9 +155,15 @@
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
             <div class="flex items-center px-4">
                 @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-                    <div class="shrink-0 me-3">
-                        <img class="h-10 w-10 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
-                    </div>
+                <div class="shrink-0 me-3">
+                    @if(!empty(auth()->user()->profile_photo_path))
+                    <img src="{{ asset('storage/'.auth()->user()->profile_photo_path) }}"
+                        alt="{{ auth()->user()->name }}" class="h-10 w-10 rounded-full object-cover">
+                    @else
+                    <img src="{{ auth()->user()->profile_photo_url }}" alt="{{ auth()->user()->name }}"
+                        class="h-10 w-10 rounded-full object-cover">
+                    @endif
+                </div>
                 @endif
 
                 <div>
